fb827de31434c10bfbec39c5ce35ab50048d76f0
Weerapat Thammawattana <weerapat.tham@gmail.com>
2018-08-09 23:33:56 +0700

    update PHP/bob almost finish

--- php/bob/bob.php
+++ php/bob/bob.php
@@ -1,1 +1,19 @@
 <?php
+
+class Bob
+{
+    function respondTo($input)
+    {
+        if(strpos($input,'?')){
+            return 'Sure.';
+        }
+        if(strtoupper($input)==$input){
+            return 'Whoa, chill out!';
+        }
+        if(strtoupper($input)==$input){
+            return 'Calm down, I know what I\'m doing!';
+        }
+
+        return 'Whatever.';
+    }
+}
