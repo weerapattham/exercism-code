30a41d99a08eb650d0fd91805ea815763d1d922e
Weerapat Thammawattana <weerapat.tham@gmail.com>
2018-08-13 21:47:35 +0700

    PHP/bob new function solution

--- php/bob/bob.php
+++ php/bob/bob.php
@@ -2,18 +2,40 @@
 
 class Bob
 {
+    function findExclamation($string)
+    {
+        return strpos($string, '!');
+    }
+    function isUpperCase($string)
+    {
+        return strtoupper($string)==$string;
+    }
+
+
     function respondTo($input)
     {
-        if(strpos($input,'?')){
-            return 'Sure.';
-        }
-        if(strtoupper($input)==$input){
+        if($this->isUpperCase($input) || $this->findExclamation())
+        {
             return 'Whoa, chill out!';
         }
-        if(strtoupper($input)==$input){
-            return 'Calm down, I know what I\'m doing!';
-        }
 
-        return 'Whatever.';
+//        if(strtoupper($input)==$input){
+//            return 'Whoa, chill out!';
+//        }
+
+//        if(strtoupper($input)==$input){
+//            return 'Whoa, chill out!';
+//        }
+//        if(strtoupper($input)==$input){
+//            return 'Calm down, I know what I\'m doing!';
+//        }
+//        if(is_numeric($input)){
+//            return 'Whatever.';
+//        }
+//        if(strpos($input,'?')){
+//            return 'Sure.';
+//        }
+//
+//        return 'Whatever.';
     }
 }
